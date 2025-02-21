91f7b87ddc0964fe30d3ba721eebb7cedb52bcdc
Andy Weeger <andy.weeger@hnu.de>
2025-02-21 13:51:22 +0100

    Diverse ST 25 prep

---
+++ docs/lib/theme/min-height.js
@@ -0,0 +1,16 @@
+<script type="text/javascript" charset="UTF-8">
+document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({
+  "notice_banner_type":"interstitial",
+  "consent_type":"express",
+  "palette":"dark",
+  "language":"en",
+  "page_load_consent_levels":["strictly-necessary"],
+  "notice_banner_reject_button_hide":false,
+  "preferences_center_close_button_hide":false,
+  "website_name":""
+  ,
+"language":"en"
+  });
+});
+</script>
